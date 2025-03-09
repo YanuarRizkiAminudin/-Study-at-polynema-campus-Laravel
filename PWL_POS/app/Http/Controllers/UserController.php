2803fff7dc851ce85310498d085b07f530aecadf
yanuarrizkia <yanuaraminudin41@smk.belajar.id>
2025-03-09 17:31:13 +0700

    Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-
+use Illuminate\Support\Js;
 class UserController extends Controller
 {
     public function index()
@@ -61,13 +61,13 @@ class UserController extends Controller
             //$user->save();
             //return view('user', ['data' => $user]); // Mengirimkan data pengguna ke view
              //===========================================================//
-        $user = UserModel::Create([
-            'username' => 'manager24',
-            'nama' => 'manager19',
-            'password' => Hash::make('123452'),
-            'level_id' => 2,
-        ]);
-        $user->username = 'manager34';
+        //$user = UserModel::Create([
+          //  'username' => 'manager24',
+            //'nama' => 'manager19',
+            //'password' => Hash::make('123452'),
+            //'level_id' => 2,
+        //]);
+        //$user->username = 'manager34';
 
         //$user->isDirty(); //true
         //$user->isDirty('username');//true
@@ -85,10 +85,67 @@ class UserController extends Controller
         //dd($user->isDirty());
         //dd($user->isClean());
         //===========================================================//
-        $user->wasChanged();//true
-        $user->wasChanged('username');//true
-        $user->wasChanged(['username','level_id']);//true
-        $user->wasChanged('nama');//false
-        dd($user->wasChanged(['nama','username']));//true
+        //$user = UserModel::Create([
+          //'username' => 'manager24',
+          //'nama' => 'manager19',
+          //'password' => Hash::make('123452'),
+          //'level_id' => 2,
+      //]);
+      //$user->username = 'manager34';
+        //$user->wasChanged();//true
+        //$user->wasChanged('username');//true
+        //$user->wasChanged(['username','level_id']);//true
+        //$user->wasChanged('nama');//false
+        //dd($user->wasChanged(['nama','username']));//true
+        //===========================================================//
+     //   $user = UserModel::all();
+      //  return view('user',['data'=> $user]);
+      $user = UserModel::where('level_id', 2)->count();
+      $user = UserModel::with('level')->get();
+
+      return view('user', ['data' => $user]);
+   }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+        return redirect('/user');
     }
 }
--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\KetegoriController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -15,11 +15,17 @@ use App\Http\Controllers\UserController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KetegoriController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
+
+//JS4
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
