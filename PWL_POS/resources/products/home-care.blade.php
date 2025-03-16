91268dda385ef68657b39ec557c9ad10bddf3152
yanuarrizkia <yanuaraminudin41@smk.belajar.id>
2025-03-17 02:12:01 +0700

    PWL_5

---
+++ PWL_POS/resources/products/home-care.blade.php
@@ -0,0 +1,6 @@
+<html>
+<head><title>Home Care</title></head>
+<body>
+    <h1>Welcome to Home Care Products</h1>
+</body>
+</html>
--- PWL_POS/routes/web.php
+++ PWL_POS/routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\SalesController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Controller;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,33 +19,52 @@ use Illuminate\Support\Facades\Controller;
 |
 */
 
-// =========== Jobsheet 3 ============
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/level', [LevelController::class, 'index']);
-// Route::get('/kategori', [KategoriController::class, 'index']);
-// Route::get('/user', [UserController::class, 'index']);
-
-// =========== Jobsheet 2 ===========
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Route ini digunakan untuk homepage
-Route::get('/', [HomeController::class, 'index']);
+// Route Home
+// Route::get('/', [HomeController::class, 'index']);
 
-// Route ini digunakan untuk halaman produk
+// Route Products (dengan prefix "category")
 Route::prefix('category')->group(function () {
     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
     Route::get('/home-care', [ProductController::class, 'homeCare']);
     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-    });
+});
 
-// Route ini digunakan untuk user dengan parameter
+// Route User (menggunakan parameter)
 Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
 
-// Route ini digunakan untuk halaman penjualan
+// Route Penjualan (POS)
 Route::get('/sales', [SalesController::class, 'index']);
+
+// Route LevelController
+Route::get('/level', [LevelController::class, 'index']);
+
+// Route KategoriController
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+// Route UserController
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+
+Route::post('/kategori', [KategoriController::class, 'store']);
+
+Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+
+Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+
+Route::post('/kategori/delete/{id}', [KategoriController::class, 'destroy'])->name('kategori.destroy');
