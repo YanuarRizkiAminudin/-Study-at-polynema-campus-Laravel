04e288aacf25d960d28ea6ded8e46ccb63dabc2e
yanuarrizkia <yanuaraminudin41@smk.belajar.id>
2025-03-24 03:24:27 +0700

    Praktikum

---
+++ Week5.2/Praktikum/app/Providers/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.template')
+
+@section('title', 'Dashboard')
+@section('breadcrumb-title', 'Dashboard')
+
+@section('content')
+<div class="container">
+    <h1>Selamat Datang di Dashboard!</h1>
+    <p>Ini adalah halaman dashboard.</p>
+</div>
+@endsection
