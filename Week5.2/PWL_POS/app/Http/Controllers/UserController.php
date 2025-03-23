bb85d1835c2e6fef19341959ff9af23409e40c32
yanuarrizkia <yanuaraminudin41@smk.belajar.id>
2025-03-24 01:59:51 +0700

    Ajax Form (AdminLTE) dan Client Validation

--- Week5.2/PWL_POS/app/Http/Controllers/UserController.php
+++ Week5.2/PWL_POS/app/Http/Controllers/UserController.php
@@ -82,7 +82,8 @@ public function show(string $id)
         ];
 
         $activeMenu = 'user'; // set menu yang sedang aktif
-        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]); 
+        $level =LevelModel::all();
+        return view('user.index', ['breadcrumb' => $breadcrumb, 'page' => $page,'level'=>$level, 'activeMenu' => $activeMenu]); 
     }
     
     // Ambil data user dalam bentuk json untuk datatables
@@ -91,7 +92,11 @@ public function list(Request $request)
     $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
         ->with('level');
 
-    return DataTables::of($users)
+        if($request->level_id){
+            $users->where('level_id', $request->level_id);
+        }
+
+    return DataTables::of(source: $users)
         // menambahkan kolom index / no urut (default nama kolom: DT RowIndex)
         ->addIndexColumn()
         ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
