e0888c2b0448b05dcb358b7e080e857f5f17f5ac
yanuarrizkia <yanuaraminudin41@smk.belajar.id>
2025-04-11 12:55:56 +0700

    isDirty,isClean,dd

--- PWL_POS1/app/Http/Controllers/UserController.php
+++ PWL_POS1/app/Http/Controllers/UserController.php
@@ -85,16 +85,41 @@ class UserController extends Controller
         //         'nama'=> 'Manager',
         //     ],
         // );
-        $user =UserModel::firstOrNew(
-            [
-                'username'=>'manager33',
-                'nama'=> 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+        // $user =UserModel::firstOrNew(
+        //     [
+        //         'username'=>'manager33',
+        //         'nama'=> 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+
+        // return view('user', ['data' => $user]);//Main
+
+        //Praktikum 2.5 – Attribute Changes
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]) ;
+        $user->username = 'manager56';
+
+        $user->isDirty();//true
+        $user->isDirty('username');//true
+        $user->isDirty('nama');//false
+        $user->isDirty(['nama', 'username']); //true
+
+        $user->isClean(); //false
+        $user->isClean('username');//true
+        $user->isClean('nama'); //true
+        $user->isClean(['nama', 'username']); //false
+
         $user->save();
 
-        return view('user', ['data' => $user]);//Main
+        $user->isDirty(); //false
+        $user->isClean(); //true
+        dd($user->isDirty());
     }
 }
