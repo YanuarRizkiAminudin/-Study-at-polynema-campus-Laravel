2055ef1d760d9999dcd39a6dffcbecbb6327be71
yanuarrizkia <yanuaraminudin41@smk.belajar.id>
2025-04-11 11:49:21 +0700

    password, level_id, dan save

--- PWL_POS1/app/Http/Controllers/UserController.php
+++ PWL_POS1/app/Http/Controllers/UserController.php
@@ -71,14 +71,30 @@ class UserController extends Controller
         //$jumlahPengguna = UserModel::where('level_id',2)->count();
         //return view('user', ['jumlah' => $jumlahPengguna]);
 
-        $user =UserModel::firstOrCreate(
+        // $user =UserModel::firstOrCreate(
+        //     [
+        //         'username'=>'manager22',
+        //         'nama'=> 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user =UserModel::firstOrNew(
+        //     [
+        //         'username'=>'manager',
+        //         'nama'=> 'Manager',
+        //     ],
+        // );
+        $user =UserModel::firstOrNew(
             [
-                'username'=>'manager22',
-                'nama'=> 'Manager Dua Dua',
+                'username'=>'manager33',
+                'nama'=> 'Manager Tiga Tiga',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
+        $user->save();
+
         return view('user', ['data' => $user]);//Main
     }
 }
