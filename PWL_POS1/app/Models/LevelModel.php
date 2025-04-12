46716b8f563825444ab3c985a9b57d8de7c0f2bf
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-13 03:43:06 +0700

    LevelModel

--- PWL_POS1/app/Models/LevelModel.php
+++ PWL_POS1/app/Models/LevelModel.php
@@ -4,12 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class LevelModel extends Model
 {
     use HasFactory;
-
-    protected $table = 'm_level'; // pastikan ini sesuai nama tabel
+//================ jobsheet 4 =====================
+// diberikannya file model baru bernama LevelModel.php
+    protected $table = 'm_level'; 
     protected $primaryKey = 'level_id';
-    protected $fillable = ['level_kode', 'level_nama'];
+
+
+    protected $fillable = [
+        'level_nama', 
+        'level_kode'
+    ];
+
+    public function user(): HasMany
+    {
+        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
+    }
 }
