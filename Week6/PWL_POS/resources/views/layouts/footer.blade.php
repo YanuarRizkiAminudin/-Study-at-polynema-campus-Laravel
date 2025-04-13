03657442c06f4defd2b34ef6b0bbf6beaeacd65b
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-13 12:13:45 +0700

    footer.blade

--- Week6/PWL_POS/resources/views/layouts/footer.blade.php
+++ Week6/PWL_POS/resources/views/layouts/footer.blade.php
@@ -1,12 +1,12 @@
-<span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-</h3>
-<p class="text-sm">I got your message bro</p>
-<p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-</div>
-</div>
-<!-- Message End -->
-</a>
-<div class="dropdown-divider"></div>
-<a href="#" class="dropdown-item">
-<!-- Message Start -->
-<div class="media">
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
