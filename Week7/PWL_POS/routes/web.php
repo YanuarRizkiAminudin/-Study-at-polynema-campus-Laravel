93bf69aee2588e0272fdaf19d35004d2d929ac11
yanuarrizkia <yanuaraminudin41@smk.belajar.id>
2025-04-14 01:27:32 +0700

    praktikum 1

--- Week7/PWL_POS/routes/web.php
+++ Week7/PWL_POS/routes/web.php
@@ -1,18 +1,44 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus berupa angka
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'postlogin']);
+Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
+
+Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
+    // masukkan semua route yang perlu autentikasi di sini
+});
+
+// artinya semua route di dalam group ini harus punya role ADM (Administrator)
+Route::middleware(['authorize:ADM'])->group(function () {
+    Route::get('/barang', [BarangController::class, 'index']);
+    Route::post('/barang/list', [BarangController::class, 'list']);
+    Route::get('/barang/create_ajax', [BarangController::class, 'create_ajax']); // ajax form create
+    Route::post('/barang/store_ajax', [BarangController::class, 'store_ajax']); // ajax store
+    Route::get('/barang/{id}/edit_ajax', [BarangController::class, 'edit_ajax']); // ajax form edit
+    Route::put('/barang/{id}/update_ajax', [BarangController::class, 'update_ajax']); // ajax update
+    Route::get('/barang/{id}/delete_ajax', [BarangController::class, 'confirm_delete_ajax']); // ajax form confirm delete
+    Route::delete('/barang/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // ajax delete
+});
+
+// artinya semua route di dalam group ini harus punya role MNG (Manager)
+Route::middleware(['authorize:MNG'])->group(function () {
+    Route::get('/barang', [BarangController::class, 'index']);
+    Route::post('/barang/list', [BarangController::class, 'list']);
+    Route::get('/barang/create_ajax', [BarangController::class, 'create_ajax']); // ajax form create
+    Route::post('/barang/store_ajax', [BarangController::class, 'store_ajax']); // ajax store
+    Route::get('/barang/{id}/edit_ajax', [BarangController::class, 'edit_ajax']); // ajax form edit
+    Route::put('/barang/{id}/update_ajax', [BarangController::class, 'update_ajax']); // ajax update
+    Route::get('/barang/{id}/delete_ajax', [BarangController::class, 'confirm_delete_ajax']); // ajax form confirm delete
+    Route::delete('/barang/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); // ajax delete
 });
