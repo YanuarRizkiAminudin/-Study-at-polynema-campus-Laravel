24a1e31a11385d578aca51b8ab3ee5b2ed6235fc
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-13 15:32:21 +0700

    userModel

--- Week6/PWL_POS/app/Http/Controllers/UserController.php
+++ Week6/PWL_POS/app/Http/Controllers/UserController.php
@@ -94,26 +94,6 @@ class UserController extends Controller
     // Ambil data user dalam bentuk json untuk datatables
     public function list(Request $request)
     {
-        // $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
-        //     ->with('level');
-
-        //     if($request->level_id){
-        //         $users->where('level_id', $request->level_id);
-        //     }
-
-        // return DataTables::of(source: $users)
-        //     // menambahkan kolom index / no urut (default nama kolom: DT RowIndex)
-        //     ->addIndexColumn()
-        //     ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
-        //         $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a>';
-        //         $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a>';
-        //         $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/' . $user->user_id) . '">'
-        //             . csrf_field() . method_field('DELETE') .
-        //             '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
-        //         return $btn;
-        //     })
-        //     ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
-        //     ->make(true);
 
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
@@ -126,16 +106,7 @@ class UserController extends Controller
         return DataTables::of($users)
             ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
-                // Tambahkan kode aksi di sini
-                /* 
-        $btn = '<a href="'.url('/user/' . $user->user_id).'" class="btn btn-info btn-sm">Detail</a> ';
-        $btn .= '<a href="'.url('/user/' . $user->user_id . '/edit').'" class="btn btn-warning btn-sm">Edit</a> ';
-        $btn .= '<form class="d-inline-block" method="POST" action="'. url('/user/'.$user->user_id).'">' . 
-        csrf_field() . 
-        method_field('DELETE') . 
-        '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button>
-        </form>';
-*/
+ 
 
                 $btn = '<button onclick="modalAction(\'' . url('/user/' . $user->user_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
                 $btn .= '<button onclick="modalAction(\'' . url('/user/' . $user->user_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
--- Week6/PWL_POS/routes/web.php
+++ Week6/PWL_POS/routes/web.php
@@ -1,23 +1,35 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-// Jobsheet 6 Prak-1 No-6
-// Route User
-Route::group(['prefix' => 'user'], function() {
-    Route::get('/', [UserController::class, 'index']);          // Menampilkan halaman awal user
-    Route::post('/list', [UserController::class, 'list']);      // Menampilkan data user dalam bentuk JSON untuk DataTables
-    Route::get('/create', [UserController::class, 'create']);   // Menampilkan halaman form tambah user
-    Route::post('/', [UserController::class, 'store']);         // Menyimpan data user baru
-    Route::get('/create_ajax', [UserController::class, 'create_ajax']);     // Menampilkan halaman form tambah user AJAX
-    Route::post('/ajax', [UserController::class, 'store_ajax']);           // Menyimpan data user baru AJAX
-    Route::get('/{id}', [UserController::class, 'show']);       // Menampilkan detail user
-    Route::get('/{id}/edit', [UserController::class, 'edit']);  // Menampilkan halaman form edit user
-    Route::put('/{id}', [UserController::class, 'update']);     // Menyimpan perubahan data user
-    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);       // Menampilkan halaman form edit user AJAX
-    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);   // Menyimpan perubahan data user AJAX
-    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);  // Menampilkan form konfirmasi hapus user AJAX
-    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // Menghapus data user AJAX
-    Route::delete('/{id}', [UserController::class, 'destroy']); // Menghapus data user
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'],function() {
+Route::get('/', [UserController::class, 'index']);               // menampilkan halaman awal user
+Route::post('/list', [UserController::class, 'list']);             // menampilkan data user dalam bentuk json untuk datatables
+Route::get('/create', [UserController::class, 'create']);         // menampilkan halaman form tambah user
+Route::post('/', [UserController::class, 'store']);              // menyimpan data user baru
+Route::get('/create_ajax', [UserController::class, 'create_ajax']);
+Route::get('/ajax', [UserController::class, 'ajax']);
+Route::get('/{id}', [UserController::class, 'show']);             // menampilkan detail user
+Route::get('/{id}/edit', [UserController::class, 'edit']);        // menampilkan halaman form edit user
+Route::put('/{id}', [UserController::class, 'update']);            // menyimpan perubahan data user
+Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
+Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
+Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
+Route::delete('/{id}', [UserController::class, 'destroy']);        // menghapus data user
 });
