283784f15ab5e267ea1097257c1f5276137bbd44
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-13 16:49:11 +0700

    BarangController

--- PWL_POS1/app/Http/Controllers/BarangController.php
+++ PWL_POS1/app/Http/Controllers/BarangController.php
@@ -6,6 +6,7 @@ use App\Models\BarangModel;
 use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -28,31 +29,40 @@ class BarangController extends Controller
         return view('barang.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'kategori' => $kategori, 'activeMenu' => $activeMenu]);
     }
 
-    // Ambil data barang dalam bentuk json untuk datatables
-public function list(Request $request)
-{
-    $barangs = BarangModel::select('barang_id', 'barang_kode', 'barang_nama', 'kategori_id')
-    ->with('kategori');
+   // Ambil data barang dalam bentuk json untuk datatables
+   public function list(Request $request)
+   {
+       $barangs = BarangModel::select('barang_id', 'barang_kode', 'barang_nama', 'kategori_id')
+                       ->with('kategori');
 
-    //filter data user berdasarkan kategori_id
-    if ($request->kategori_id) {
-        $barangs->where('kategori_id', $request->kategori_id);
-    }
+       //filter data user berdasarkan kategori_id
+       if ($request->kategori_id) {
+           $barangs->where('kategori_id', $request->kategori_id);
+       }
 
-    return DataTables::of($barangs)
-        // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
-        ->addIndexColumn()
-        ->addColumn('aksi', function ($barang) { // menambahkan kolom aksi
-            $btn = '<a href="' . url('/barang/' . $barang->barang_id) . '" class="btn btn-info btn-sm">Detail</a> ';
-            $btn .= '<a href="' . url('/barang/' . $barang->barang_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
-            $btn .= '<form class="d-inline-block" method="POST" action="' . url('/barang/' . $barang->barang_id) . '">'
-                . csrf_field() . method_field('DELETE') .
-                '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
-            return $btn;
-        })
-        ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
-        ->make(true);
-}
+       return DataTables::of($barangs)
+           // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
+           ->addIndexColumn()
+           // menambahkan kolom aksi
+           ->addColumn('aksi', function ($barang) {
+               // $btn = '<a href="' . url('/barang/' . $barang->barang_id) . '" class="btn btn-info btn-sm">Detail</a>';
+               // $btn .= ' <a href="' . url('/barang/' . $barang->barang_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a>';
+               // $btn .= ' <form class="d-inline-block" method="POST" action="' . url('/barang/' . $barang->barang_id) . '">'
+               //     . csrf_field()
+               //     . method_field('DELETE')
+               //     . '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\')">Hapus</button>'
+               //     . '</form>';
+
+               // JS6 - Tugas(m_barang)
+               $btn  = '<button onclick="modalAction(\''.url('/barang/' . $barang->barang_id . '/show_ajax').'\')" class="btn btn-info btn-sm">Detail</button> '; 
+               $btn .= '<button onclick="modalAction(\''.url('/barang/' . $barang->barang_id . '/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button> ';
+               $btn .= '<button onclick="modalAction(\''.url('/barang/' . $barang->barang_id . '/delete_ajax').'\')"  class="btn btn-danger btn-sm">Hapus</button> '; 
+
+               return $btn;
+           })
+           ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
+           ->make(true);
+   }
 
 // Menampilkan halaman form tambah kategori 
 public function create(){
@@ -166,12 +176,13 @@ public function destroy(string $id)
     try {
         BarangModel::destroy($id);    // hapus data kategori
 
-        return redirect('/barang')->with('success', 'Data barang berhasil dihapus');
+        return redirect('/barang')->with('success', 'Data kategori berhasil dihapus');
     } catch (\Illuminate\Database\QueryException $e) {
         //Jika terjadi error ketika menghapus data, redirect kembali ke halaman dengan membawa pesan error
-        return redirect('/barang')->with('error', 'Data barang gagal dihapus karena masuh terdapat tabel lain yang terkait dengan data ini');
+        return redirect('/barang')->with('error', 'Data gagal dihapus karena masuh terdapat tabel lain yang terkait dengan data ini');
     }
 }
+
 public function create_ajax() {
     $kategori = KategoriModel::select('kategori_id', 'kategori_nama')->get();
 
