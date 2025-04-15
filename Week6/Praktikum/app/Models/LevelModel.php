51e2467f251b3c12e22df1fcedfe9287c21d21f1
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-16 05:40:24 +0700

    praktikum

--- Week6/Praktikum/app/Models/LevelModel.php
+++ Week6/Praktikum/app/Models/LevelModel.php
@@ -4,14 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 class LevelModel extends Model
 {
     use HasFactory;
-
-    protected $table = 'm_level';
+//================ jobsheet 4 =====================
+// diberikannya file model baru bernama LevelModel.php
+    protected $table = 'm_level'; 
     protected $primaryKey = 'level_id';
 
 
-protected $fillable = ['level_nama']; // Sesuaikan dengan kolom yang ada di tabel m_level
+    protected $fillable = [
+        'level_id',
+        'level_nama', 
+        'level_kode'
+    ];
+
+    public function user(): HasMany
+    {
+        return $this->hasMany(UserModel::class, 'level_id', 'level_id');
+    }
 }
