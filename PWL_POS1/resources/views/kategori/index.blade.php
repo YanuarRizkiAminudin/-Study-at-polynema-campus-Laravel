b9339a5467189a23f1bb73ffd559fadb495f9231
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-20 19:18:19 +0700

    Implementasi Export PDF dam import

--- PWL_POS1/resources/views/kategori/index.blade.php
+++ PWL_POS1/resources/views/kategori/index.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.template')
 
 @section('content')
-<div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
     <div class="card card-outline card-primary">
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
-            <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}"> Tambah </a>
-                <!-- <a class="btn btn-sm btn-primary mt-1" href="{{ url('kategori/create') }}"> Tambah </a> -->
-                <button onclick="modalAction('{{ url('/kategori/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+                <a class="btn btn-sm btn-primary" href="{{ url('kategori/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('/kategori/create_ajax') }}')" class="btn btn-sm btn-success">
+                    Tambah Ajax
+                </button>
+                <button onclick="modalAction('{{ url('/kategori/import') }}')" class="btn btn-sm btn-info">
+                    Import Data
+                </button>
+                <a href="{{ url('kategori/export_excel') }}" class="btn btn-sm btn-primary">
+                    <i class="fa fa-file-excel"></i>
+                    Export Excel
+                </a>
+                <a href="{{ url('kategori/export_pdf') }}" class="btn btn-sm btn-warning">
+                    <i class="fa fa-file-pdf"></i>
+                    Export PDF
+                </a>
             </div>
         </div>
         <div class="card-body">
@@ -21,15 +32,17 @@
             <table class="table table-bordered table-striped table-hover table-sm" id="table_kategori">
                 <thead>
                     <tr>
-                        <th>Kategori ID</th>
-                        <th>Kategori Kode</th>
-                        <th>Kategori Nama</th>
+                        <th>No</th>
+                        <th>Kode Kategori</th>
+                        <th>Nama Kategori</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
             </table>
         </div>
     </div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
 @push('css')
@@ -37,48 +50,49 @@
 
 @push('js')
     <script>
-    function modalAction(url = ''){
-        $('#myModal').load(url,function(){
-            $('#myModal').modal('show');
-        });
-    }
-    var dataKategori;
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
+
+        var dataUser;
         $(document).ready(function() {
-            dataKategori = $('#table_kategori').DataTable({
-                // serverSide : true , jika ingin menggunakan server side processing
+            dataUser = $('#table_kategori').DataTable({
                 serverSide: true,
                 ajax: {
                     "url": "{{ url('kategori/list') }}",
                     "dataType": "json",
                     "type": "POST",
                 },
-                columns: [
+                columns: [{
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    },
                     {
-                        // nomor urut dari laravel datatable addIndexColumn ()
-                                      data: "DT_RowIndex",
-                                      className: "text-center",
-                                      orderable: false,
-                                      searchable: false
-                                  }, {
-                                      data: "kategori_kode",
-                                      className: "",
-                                      // orderable : true , jika ingin kolom ini bisa diurutkan
-                                      orderable: true,
-                                      // searchable : true , jika ingin kolom ini bisa dicari
-                                      searchable: true
-                                  }, {
-                                      data: "kategori_nama",
-                                      className: "",
-                                      orderable: true,
-                                      searchable: true
-                                  },{
-                                      data: "aksi",
-                                      className: "",
-                                      orderable: false,
-                                      searchable: false
-                                  }
-                              ]
-                          });
-                      });
+                        data: "kategori_kode",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "kategori_nama",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "aksi",
+                        className: "text-center",
+                        width: "195px",
+                        orderable: false,
+                        searchable: false
+                    }
+                ],
+                autoWidth: false,
+            });
+        });
     </script>
 @endpush
