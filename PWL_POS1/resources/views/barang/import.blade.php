32acf0824cb8ffd48d65a782b7293608162e6c49
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-20 18:14:20 +0700

    Implementasi Import PDF

--- PWL_POS1/resources/views/barang/import.blade.php
+++ PWL_POS1/resources/views/barang/import.blade.php
@@ -4,12 +4,14 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Import Data Barang</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <button type="button" class="close" data-dismiss="modal" aria label="Close"><span
+                        aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
                 <div class="form-group">
                     <label>Download Template</label>
-                    <a href="{{ asset('template_barang.xlsx') }}" class="btn btn-info btn-sm" download><i class="fa fa-file-excel"></i>Download</a>
+                    <a href="{{ asset('template_barang.xlsx') }}" class="btn btn-info btn-sm" download><i
+                            class="fa fa-file-excel"></i> Download</a>
                     <small id="error-kategori_id" class="error-text form-text text-danger"></small>
                 </div>
                 <div class="form-group">
@@ -25,7 +27,6 @@
         </div>
     </div>
 </form>
-
 <script>
     $(document).ready(function () {
         $("#form-import").validate({
@@ -33,23 +34,24 @@
                 file_barang: { required: true, extension: "xlsx" },
             },
             submitHandler: function (form) {
-                var formData = new FormData(form);
+                var formData = new FormData(form);  // Jadikan form ke FormData untuk menghandle file 
+
                 $.ajax({
                     url: form.action,
                     type: form.method,
-                    data: formData,
-                    processData: false,
+                    data: formData,     // Data yang dikirim berupa FormData 
+                    processData: false, // setting processData dan contentType ke false, untuk menghandle file 
                     contentType: false,
                     success: function (response) {
-                        if (response.status) {
+                        if (response.status) { // jika sukses 
                             $('#myModal').modal('hide');
                             Swal.fire({
                                 icon: 'success',
                                 title: 'Berhasil',
                                 text: response.message
                             });
-                            tableBarang.ajax.reload();
-                        } else {
+                            dataBarang.ajax.reload(); // reload datatable 
+                        } else { // jika error 
                             $('.error-text').text('');
                             $.each(response.msgField, function (prefix, val) {
                                 $('#error-' + prefix).text(val[0]);
@@ -76,5 +78,5 @@
                 $(element).removeClass('is-invalid');
             }
         });
-    });
-</script>a
+    }); 
+</script>
