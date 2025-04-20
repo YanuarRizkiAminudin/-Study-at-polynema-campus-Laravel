d83c3ce4b4d45595dd85c75727f974e6a617d662
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-20 20:08:20 +0700

    Ajax Form

--- PWL_POS1/resources/views/level/confirm_ajax.blade.php
+++ PWL_POS1/resources/views/level/confirm_ajax.blade.php
@@ -3,13 +3,14 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">                  <span aria-hidden="true">&times;</span>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <div class="alert alert-danger">
                     <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
-                    Data yang Anda cari tidak ditemukan
+                    Data yang anda cari tidak ditemukan
                 </div>
                 <a href="{{ url('/level') }}" class="btn btn-warning">Kembali</a>
             </div>
@@ -34,14 +35,13 @@
                     </div>
                     <table class="table table-sm table-bordered table-striped">
                         <tr>
-                            <th class="text-right col-3">Kode Level :</th>
+                            <th class="text-right col-3">Level Kode:</th>
                             <td class="col-9">{{ $level->level_kode }}</td>
                         </tr>
                         <tr>
-                            <th class="text-right col-3">Nama Level :</th>
+                            <th class="text-right col-3">Level Nama:</th>
                             <td class="col-9">{{ $level->level_nama }}</td>
                         </tr>
-                        
                     </table>
                 </div>
                 <div class="modal-footer">
@@ -68,8 +68,7 @@
                                     title: 'Berhasil',
                                     text: response.message
                                 });
-                                //dataUser.ajax.reload(); // Asumsi ada variabel dataUser untuk datatable
-                                
+                                dataUser.ajax.reload();
                             } else {
                                 $('.error-text').text('');
                                 $.each(response.msgField, function(prefix, val) {
@@ -81,7 +80,16 @@
                                     text: response.message
                                 });
                             }
-                        }
+                        },
+                        // custom catch error
+                        // error: function(error) {
+                        //     Swal.fire({
+                        //         icon: 'error',
+                        //         title: 'Terjadi Kesalahan',
+                        //         text: 'Gagal menghapus data: ' + error.statusText
+                        //     });
+                        // }
+                        //
                     });
                     return false;
                 },
--- PWL_POS1/resources/views/level/show_ajax.blade.php
+++ PWL_POS1/resources/views/level/show_ajax.blade.php
@@ -1,101 +1,25 @@
-@empty($level)
-    <div id="modal-master" class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Kesalahan</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="alert alert-danger">
-                    <h5><i class="icon fas fa-ban"></i> Kesalahan!!!</h5>
-                    Data yang Anda cari tidak ditemukan
-                </div>
-                <a href="{{ url('/level') }}" class="btn btn-warning">Kembali</a>
-            </div>
+<div id="modal-master" class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Data Level</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
+        <div class="modal-body">
+            <table class="table table-bordered table-striped table-hover table-sm">
+                <tr>
+                    <th>ID</th>
+                    <td>{{ $level->level_id }}</td>
+                </tr>
+                <tr>
+                    <th>Kode Level</th>
+                    <td>{{ $level->level_kode }}</td>
+                </tr>
+                <tr>
+                    <th>Nama Level</th>
+                    <td>{{ $level->level_nama }}</td>
+                </tr>
+            </table>
     </div>
-@else
-    <form action="" method="POST" id="form-show">
-        @csrf
-        @method('GET')
-        <div id="modal-master" class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Tampilkan Data Level</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="alert alert-warning">
-                        <h5><i class="icon fas fa-show"></i> Menampilkan data level</h5>
-                        
-                    </div>
-                    <table class="table table-sm table-bordered table-striped">
-                        <tr>
-                            <th class="text-right col-3">Kode Level :</th>
-                            <td class="col-9">{{ $level->level_kode }}</td>
-                        </tr>
-                        <tr>
-                            <th class="text-right col-3">Nama Level :</th>
-                            <td class="col-9">{{ $level->level_nama }}</td>
-                        </tr>
-                
-                    </table>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" data-dismiss="modal" class="btn btn-warning">Keluar</button>
-                </div>
-            </div>
-        </div>
-    </form>
-    <script>
-        // $(document).ready(function() {
-        //     $("#form-delete").validate({
-        //         rules: {},
-        //         submitHandler: function(form) {
-        //             $.ajax({
-        //                 url: form.action,
-        //                 type: form.method,
-        //                 data: $(form).serialize(),
-        //                 success: function(response) {
-        //                     if (response.status) {
-        //                         $('#myModal').modal('hide');
-        //                         Swal.fire({
-        //                             icon: 'success',
-        //                             title: 'Berhasil',
-        //                             text: response.message
-        //                         });
-        //                         dataUser.ajax.reload();
-        //                     } else {
-        //                         $('.error-text').text('');
-        //                         $.each(response.msgField, function(prefix, val) {
-        //                             $('#error-' + prefix).text(val[0]);
-        //                         });
-        //                         Swal.fire({
-        //                             icon: 'error',
-        //                             title: 'Terjadi Kesalahan',
-        //                             text: response.message
-        //                         });
-        //                     }
-        //                 }
-        //             });
-        //             return false;
-        //         },
-        //         errorElement: 'span',
-        //         errorPlacement: function(error, element) {
-        //             error.addClass('invalid-feedback');
-        //             element.closest('.form-group').append(error);
-        //         },
-        //         highlight: function(element, errorClass, validClass) {
-        //             $(element).addClass('is-invalid');
-        //         },
-        //         unhighlight: function(element, errorClass, validClass) {
-        //             $(element).removeClass('is-invalid');
-        //         }
-        //     });
-        // });
-    </script>
-@endempty
+</div>
