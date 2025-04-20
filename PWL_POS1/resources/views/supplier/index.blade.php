4962ddbbe5740389cadb1b07b1564fff6fc1cd2d
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-20 19:34:18 +0700

    Implementasi Export PDF dan import

--- PWL_POS1/resources/views/supplier/index.blade.php
+++ PWL_POS1/resources/views/supplier/index.blade.php
@@ -5,8 +5,21 @@
         <div class="card-header">
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
-                <a class="btn btn-sm btn-primary mt-1" href="{{ url('supplier/create') }}">Tambah Supplier</a>
-                <button onclick="modalAction('{{ url('supplier/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Supplier Ajax</button>
+                <a class="btn btn-sm btn-primary" href="{{ url('supplier/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('/supplier/create_ajax') }}')" class="btn btn-sm btn-success">
+                    Tambah Ajax
+                </button>
+                <button onclick="modalAction('{{ url('/supplier/import') }}')" class="btn btn-sm btn-info">
+                    Import Data
+                </button>
+                <a href="{{ url('supplier/export_excel') }}" class="btn btn-sm btn-primary">
+                    <i class="fa fa-file-excel"></i>
+                    Export Excel
+                </a>
+                <a href="{{ url('supplier/export_pdf') }}" class="btn btn-sm btn-warning">
+                    <i class="fa fa-file-pdf"></i>
+                    Export PDF
+                </a>
             </div>
         </div>
         <div class="card-body">
@@ -16,36 +29,21 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="form-group row">
-                        <label class="col-1 control-label col-form-label">Filter :</label>
-                        <div class="col-3">
-                            <select class="form-control" id="supplier_kode" name="supplier_kode">
-                                <option value="">- Semua -</option>
-                                @foreach ($suppliers as $supplier)
-                                    <option value="{{ $supplier->supplier_kode }}">{{ $supplier->supplier_kode }}</option>
-                                @endforeach
-                            </select>
-                            <small class="form-text text-muted">Kode Supplier</small>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <table class="table table-bordered table-striped table-hover table-sm" id="table_supplier">
                 <thead>
                     <tr>
-                        <th>ID</th>
+                        <th>No</th>
                         <th>Kode Supplier</th>
                         <th>Nama Supplier</th>
-                        <th>Alamat</th>
+                        <th>Alamat Supplier</th>
                         <th>Aksi</th>
                     </tr>
                 </thead>
             </table>
         </div>
     </div>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
 @push('css')
@@ -53,57 +51,54 @@
 
 @push('js')
     <script>
-        function modalAction(url = ''){
-            $('#myModal').load(url,function(){
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
                 $('#myModal').modal('show');
             });
         }
 
-        var dataSupplier;
+        var dataUser;
         $(document).ready(function() {
-            dataSupplier = $('#table_supplier').DataTable({
+            dataUser = $('#table_supplier').DataTable({
                 serverSide: true,
                 ajax: {
                     "url": "{{ url('supplier/list') }}",
-                    "type": "POST",
                     "dataType": "json",
-                    "data": function(d) {
-                        d.supplier_kode = $('#supplier_kode').val();
-                    }
+                    "type": "POST",
                 },
-                columns: [
-                    {
+                columns: [{
                         data: "DT_RowIndex",
                         className: "text-center",
                         orderable: false,
-                        searchable: false,
+                        searchable: false
                     },
                     {
                         data: "supplier_kode",
+                        className: "",
                         orderable: true,
                         searchable: true
                     },
                     {
                         data: "supplier_nama",
+                        className: "",
                         orderable: true,
                         searchable: true
                     },
                     {
                         data: "supplier_alamat",
-                        orderable: true,
+                        className: "",
+                        orderable: false,
                         searchable: true
                     },
                     {
                         data: "aksi",
                         className: "text-center",
+                        width: "195px",
                         orderable: false,
                         searchable: false
                     }
-                ]
-            });
-
-            $('#supplier_kode').on('change', function() {
-                dataSupplier.ajax.reload();
+                ],
+                autoWidth: false,
             });
         });
     </script>
