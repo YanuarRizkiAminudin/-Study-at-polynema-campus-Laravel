78e9807353a271f10092b114e457edb49a500ccb
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-22 18:10:58 +0700

    add Stok

---
+++ PWL_POS1/resources/views/stok/index.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.template')
+
+@section('content')
+    <div class="card card-outline card-primary">
+        <div class="card-header">
+            <h3 class="card-title">{{ $page->title }}</h3>
+            <div class="card-tools">
+                <a class="btn btn-sm btn-primary" href="{{ url('stok/create') }}">Tambah</a>
+                <button onclick="modalAction('{{ url('/stok/create_ajax') }}')" class="btn btn-sm btn-success">Tambah Ajax</button>
+                <button onclick="modalAction('{{ url('/stok/import') }}')" class="btn btn-sm btn-info">Import Data</button>
+                <a href="{{ url('stok/export_excel') }}" class="btn btn-sm btn-primary">
+                    <i class="fa fa-file-excel"></i>
+                    Export Excel
+                </a>
+                <a href="{{ url('stok/export_pdf') }}" class="btn btn-sm btn-warning">
+                    <i class="fa fa-file-pdf"></i>
+                    Export PDF
+                </a>
+            </div>
+        </div>
+        <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_stok">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Id Barang</th>
+                        <th>Nama Barang</th>
+                        <th>Id Supplier</th>
+                        <th>Stok Tanggal</th>
+                        <th>Stok Jumlah</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+@endsection
+
+@push('css')
+@endpush
+
+@push('js')
+    <script>
+        function modalAction(url = '') {
+            $('#myModal').load(url, function() {
+                $('#myModal').modal('show');
+            });
+        }
+
+        var dataUser;
+        $(document).ready(function() {
+            dataUser = $('#table_stok').DataTable({
+                serverSide: true,
+                ajax: {
+                    "url": "{{ url('stok/list') }}",
+                    "dataType": "json",
+                    "type": "POST",
+                },
+                columns: [{
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: "barang_id",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "barang.barang_nama",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "supplier_id",
+                        className: "",
+                        orderable: true,
+                        searchable: true
+                    },
+                    {
+                        data: "stok_tanggal",
+                        className: "",
+                        orderable: true,
+                        searchable: false
+                    },
+                    {
+                        data: "stok_jumlah",
+                        className: "",
+                        orderable: true,
+                        searchable: false
+                    }, {
+                        data: "aksi",
+                        className: "text-center",
+                        width: "195px",
+                        orderable: false,
+                        searchable: false
+                    }
+                ],
+                autoWidth: false,
+            });
+        });
+    </script>
+@endpush
