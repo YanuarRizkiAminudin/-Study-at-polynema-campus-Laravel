d5e6cb2c51d7e7514e6d575a548829365710cb78
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-27 15:53:17 +0700

    fix bug

--- PWL_POS1/app/Models/UserModel.php
+++ PWL_POS1/app/Models/UserModel.php
@@ -5,48 +5,57 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\JWTAuth\Contracts\JWTSubject;
-use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
-    public function getJWTIdentifier(){
+    public function getJWTIdentifier()
+    {
         return $this->getKey();
     }
-    public function getJWTCustomClaims(){
+
+    public function getJWTCustomClaims()
+    {
         return [];
     }
+    
     use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
-    protected $hidden = ['password']; // jangan di tampilkan saat select
-    //protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+    protected $fillable = ['level_id', 'username', 'nama', 'password', 'created_at', 'updated_at'];
 
-    /**
-     * Relasi ke tabel level
-     */
+    protected $hidden = ['password']; // jangan di tampilkan saat select 
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
+
+
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
-    /*
-    * Mendapatkan nama role
-    */
-    public function getRoleName(): string {
+
+    /**
+     * Mendapatkan nama role
+     */
+    public function getRoleName(): string
+    {
         return $this->level->level_nama;
     }
+
     /**
-     * cek apkaha user memiliki role tertentu
+     * Cek apakah user memiliki role tertentu
      */
-    public function hasRole($role):bool {
+    public function hasRole($role): bool
+    {
         return $this->level->level_kode == $role;
     }
+
     /**
-     * Mendapatakan kode role
+     * Mendapatkan kode role
      */
-    public function getRole(){
+    public function getRole()
+    {
         return $this->level->level_kode;
     }
 }
--- PWL_POS1/routes/api.php
+++ PWL_POS1/routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +21,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', RegisterController::class)->name('register');
