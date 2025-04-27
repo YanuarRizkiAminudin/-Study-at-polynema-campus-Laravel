7e3f17e592b33227b72c7f90a162847b3fbb6c92
YanuarRizkiAminudin <yanuariyan24@gmail.com>
2025-04-27 19:47:55 +0700

    CRUD API Kategori

--- PWL_POS1/app/Http/Controllers/Api/KategoriController.php
+++ PWL_POS1/app/Http/Controllers/Api/KategoriController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\KategoriModel;
 use Illuminate\Http\Request;
-
+//
 class KategoriController extends Controller
 {
     public function index()
